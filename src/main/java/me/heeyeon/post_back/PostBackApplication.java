5db3969da65bad73b1666712fb9d74f672201c60
yeon <731aa@naver.com>
2024-09-02 22:58:09 +0900

    post_back project initial settings

---
+++ src/main/java/me/heeyeon/post_back/PostBackApplication.java
@@ -0,0 +1,13 @@
+package me.heeyeon.post_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/heeyeon/post_back/PostBackApplicationTests.java
@@ -0,0 +1,13 @@
+package me.heeyeon.post_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
